e8c06bd766318f59bbccc81fb5c312726bd22d12
owahaha <1069823034@qq.com>
2020-11-14 21:41:19 +0800

    更新实验四

--- 实验四 Intent/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ 实验四 Intent/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -1,10 +1,12 @@
 package com.example.activitytest;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -22,10 +24,35 @@ public class FirstActivity extends AppCompatActivity {
                 Intent intent = new Intent("com.example.activitytest.ACTION_START");
                 intent.addCategory("com.example.activitytest.MY_CATEGORY");
        */
+      /*
                 Intent intent = new Intent(Intent.ACTION_VIEW);
                 intent.setData(Uri.parse("http://www.bilibili.com"));
+
+       */
+      /*
+                String data = "Hello SecondActivity";
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("extra_data",data);
                 startActivity(intent);
+       */
+
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent,1);
             }
+
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        switch (requestCode){
+            case 1:
+                if(resultCode == RESULT_OK){
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
--- 实验四 Intent/ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
+++ 实验四 Intent/ActivityTest/app/src/main/java/com/example/activitytest/SecondActivity.java
@@ -2,7 +2,11 @@ package com.example.activitytest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class SecondActivity extends AppCompatActivity {
 
@@ -10,5 +14,28 @@ public class SecondActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.second_layout);
+        /*
+        Intent intent = getIntent();
+        String data = intent.getStringExtra("extra_data");
+        Log.d("SecondActivity", data);
+         */
+        Button button2 = (Button) findViewById(R.id.button_2);
+        button2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.putExtra("data_return","Hello FirstActivity");
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("data_return","Hello FristActivity");
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
