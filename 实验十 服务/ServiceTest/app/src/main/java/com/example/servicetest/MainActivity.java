7cd989fcf3eed7790781c5a25463144e8a1f4b2d
owahaha <1069823034@qq.com>
2020-11-28 19:42:01 +0800

    更新 实验十内容

--- 实验十 服务/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
+++ 实验十 服务/ServiceTest/app/src/main/java/com/example/servicetest/MainActivity.java
@@ -2,13 +2,36 @@ package com.example.servicetest;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button startService = (Button) findViewById(R.id.start_service);
+        Button stopService = (Button) findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);
+                break;
+            default:
+                break;
+        }
     }
 }
