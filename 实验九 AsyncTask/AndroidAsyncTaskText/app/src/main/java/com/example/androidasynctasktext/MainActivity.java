75ed738864570527e0577a9188cee1a5a461a256
owahaha <1069823034@qq.com>
2020-11-28 16:27:44 +0800

    更新 实验九内容

--- 实验九 AsyncTask/AndroidAsyncTaskText/app/src/main/java/com/example/androidasynctasktext/MainActivity.java
+++ 实验九 AsyncTask/AndroidAsyncTaskText/app/src/main/java/com/example/androidasynctasktext/MainActivity.java
@@ -1,13 +1,77 @@
 package com.example.androidasynctasktext;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    MyTask mTask;
+    Button button;
+    TextView text;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.button);
+        text = (TextView) findViewById(R.id.text);
+        progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+        mTask = new MyTask();
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mTask.execute();
+            }
+        });
+
     }
+
+
+    private class MyTask extends AsyncTask<String, Integer, String> {
+        @Override
+        protected void onPreExecute() {
+            text.setText("加载中");
+        }
+        @Override
+        protected String doInBackground(String... params) {
+
+            try {
+                int count = 0;
+                int length = 1;
+                while (count<99) {
+                    count += length;
+                    publishProgress(count);
+                    Thread.sleep(50);
+                }
+            }catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(Integer... progresses) {
+
+            progressBar.setProgress(progresses[0]);
+            text.setText("loading..." + progresses[0] + "%");
+
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            text.setText("加载完毕");
+        }
+
+    }
+
 }
