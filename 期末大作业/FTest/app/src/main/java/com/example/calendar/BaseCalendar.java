f568ce7598685a7ef6c4db04137d70427d8ea8ed
owahaha <1069823034@qq.com>
2020-12-13 16:14:55 +0800

    更新项目

--- 期末大作业/FTest/app/src/main/java/com/example/calendar/BaseCalendar.java
+++
@@ -1,6 +0,0 @@
-package com.example.calendar;
-
-import androidx.viewpager.widget.ViewPager;
-
-public abstract class BaseCalendar extends ViewPager implements ICalendar {
-}
--- 期末大作业/FTest/app/src/main/java/com/example/listener/OnCalendarChangedListener.java
+++
@@ -1,9 +0,0 @@
-package com.example.listener;
-
-import com.example.calendar.BaseCalendar;
-
-import java.time.LocalDate;
-
-public interface OnCalendarChangedListener {
-    void onCalendarChange(BaseCalendar baseCalendar, int year, int month, LocalDate localDate);
-}
